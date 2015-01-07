d39569a635bfffe576e2ba96baabf62d1bb4dc52
Si Te Feng <sfeng@A1070-MBP15.local>
2015-01-06 17:10:48 -0800

    Initial Commit

---
+++ HappyTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HappyTracker
+//
+//  Created by Si Te Feng on 1/6/15.
+//  Copyright (c) 2015 Si Te Feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
